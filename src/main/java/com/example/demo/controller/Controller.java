d3aff0c8e33ca2ec2a6dd872c1f63b3b50737ae3
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-17 22:57:44 +0800

    Removed old codes hidden in comments after checking the code is running

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -23,30 +23,6 @@ public class Controller implements PropertyChangeListener {
 		this.stage = stage;
 	}
 
-	/**
-	 public void launchGame() throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-	 InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException  {
-	 stage.show();
-	 goToLevel(LEVEL_ONE_CLASS_NAME);
-	 }
-	 */
-
-	/**
-	 * private void goToLevel(String className) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-	 * InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-	 * Class<?> myClass = Class.forName(className);
-	 * Constructor<?> constructor = myClass.getConstructor(double.class, double.class);
-	 * LevelParent myLevel = (LevelParent) constructor.newInstance(stage.getHeight(), stage.getWidth());
-	 * <p>
-	 * // Add this controller as a PropertyChangeListener
-	 * myLevel.addPropertyChangeListener(this);
-	 * myLevel.removePropertyChangeListener(this);
-	 * Scene scene = myLevel.initializeScene();
-	 * stage.setScene(scene);
-	 * myLevel.startGame();
-	 * }
-	 */
-
 	public void launchGame(Stage stage) throws CustomException {
 		try {
 			stage.show();
