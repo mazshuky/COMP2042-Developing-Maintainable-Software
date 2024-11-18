aea60af7ff90e27b5dae7cd47a195617e1e31f6a
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-18 15:16:55 +0800

    Added Javadoc comments in ActiveActorDestructible class

--- src/main/java/com/example/demo/ActiveActorDestructible.java
+++ src/main/java/com/example/demo/ActiveActorDestructible.java
@@ -1,31 +1,64 @@
 package com.example.demo;
 
+/**
+ * Abstract class representing a destructible active actor in the game
+ */
 public abstract class ActiveActorDestructible extends ActiveActor implements Destructible {
 
 	private boolean isDestroyed;
+
+	/**
+	 * Constructs a new ActiveActorDestructible
+	 * @param imageName     the name of the image file
+	 * @param imageHeight   the height of the image
+	 * @param initialXPos   the initial x position
+	 * @param initialYPos   the initial y position
+	 */
 
 	public ActiveActorDestructible(String imageName, int imageHeight, double initialXPos, double initialYPos) {
 		super(imageName, imageHeight, initialXPos, initialYPos);
 		isDestroyed = false;
 	}
 
+	/**
+	 * Updates the position of the actor
+	 */
 	@Override
 	public abstract void updatePosition();
 
+	/**
+	 * Updates the state of the actor
+	 */
 	public abstract void updateActor();
 
+	/**
+	 * Applies damage to the actor
+	 */
 	@Override
 	public abstract void takeDamage();
 
+	/**
+	 * Destroys the actor
+	 */
 	@Override
 	public void destroy() {
 		setDestroyed(true);
 	}
 
-	protected void setDestroyed(boolean isDestroyed) {
-		this.isDestroyed = isDestroyed;
+	/**
+	 * Sets the destroyed state of the actor
+	 *
+	 * @param isDestroyed   the new destroyed state
+	 */
+    protected void setDestroyed(boolean isDestroyed) {
+	 	this.isDestroyed = isDestroyed;
 	}
 
+	/**
+	 * Checks if the actor is destroyed
+	 *
+	 * @return  true if the actor is destroyed, false otherwise
+	 */
 	public boolean isDestroyed() {
 		return isDestroyed;
 	}
