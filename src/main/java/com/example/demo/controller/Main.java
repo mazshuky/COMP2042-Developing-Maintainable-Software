73989e70b684038477a3385ddb45711dbe945441
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-18 15:10:24 +0800

    Added Javadoc comments in Main class

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Main class that launches the game
+ */
 public class Main extends Application {
 
 	private static final int SCREEN_WIDTH = 1300;
@@ -10,6 +13,12 @@ public class Main extends Application {
 	private static final String TITLE = "Sky Battle";
 	private Controller myController;
 
+	/**
+	 * Starts the JavaFX application
+	 *
+	 * @param stage               the primary stage for the application
+	 * @throws CustomException    if there is an error launching the game
+	 */
 	@Override
 	public void start(Stage stage) throws CustomException {
 		try {
@@ -19,11 +28,17 @@ public class Main extends Application {
 			stage.setWidth(SCREEN_WIDTH);
 			myController = new Controller(stage);
 			myController.launchGame(stage);
+
 		} catch (SecurityException | IllegalArgumentException e) {
 			throw new CustomException("Error launching game", e);
 		}
 	}
 
+	/**
+	 * Main method that launches the game
+	 *
+	 * @param args    the command line arguments
+	 */
 	public static void main(String[] args) {
 		launch();
 	}
