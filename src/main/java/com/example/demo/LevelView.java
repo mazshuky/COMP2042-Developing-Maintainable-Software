75c9805e3d60504522280df975dfe677c816d010
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-18 14:13:41 +0800

    Added JavaDoc comments for class and methods

--- src/main/java/com/example/demo/LevelView.java
+++ src/main/java/com/example/demo/LevelView.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import javafx.scene.Group;
 
+// Class representing the view of the game level
 public class LevelView {
 	
 	private static final double HEART_DISPLAY_X_POSITION = 5;
@@ -10,31 +11,46 @@ public class LevelView {
 	private static final int WIN_IMAGE_Y_POSITION = 175;
 	private static final int LOSS_SCREEN_X_POSITION = -160;
 	private static final int LOSS_SCREEN_Y_POSITION = -375;
+
 	private final Group root;
 	private final WinImage winImage;
 	private final GameOverImage gameOverImage;
 	private final HeartDisplay heartDisplay;
-	
+
+	/**
+	 * Constructs a new LevelView
+	 *
+	 * @param root              the root group for the scene
+	 * @param heartsToDisplay   the initial number of hearts to display
+	 */
 	public LevelView(Group root, int heartsToDisplay) {
 		this.root = root;
 		this.heartDisplay = new HeartDisplay(HEART_DISPLAY_X_POSITION, HEART_DISPLAY_Y_POSITION, heartsToDisplay);
 		this.winImage = new WinImage(WIN_IMAGE_X_POSITION, WIN_IMAGE_Y_POSITION);
 		this.gameOverImage = new GameOverImage(LOSS_SCREEN_X_POSITION, LOSS_SCREEN_Y_POSITION);
 	}
-	
+
+	// Shows the heart display on the screen
 	public void showHeartDisplay() {
 		root.getChildren().add(heartDisplay.getContainer());
 	}
 
+	// Displays the win image on the screen
 	public void showWinImage() {
 		root.getChildren().add(winImage);
 		winImage.showWinImage();
 	}
-	
+
+	// Displays the game over image on the screen
 	public void showGameOverImage() {
 		root.getChildren().add(gameOverImage);
 	}
-	
+
+	/**
+	 * Removes hearts from the heart display based on remaining hearts
+	 *
+	 * @param heartsRemaining
+	 */
 	public void removeHearts(int heartsRemaining) {
 		int currentNumberOfHearts = heartDisplay.getContainer().getChildren().size();
 		for (int i = 0; i < currentNumberOfHearts - heartsRemaining; i++) {
