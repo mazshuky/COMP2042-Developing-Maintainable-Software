42988b624cceaab50dd217d1ec02910a2bb3437e
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-22 14:44:01 +0800

    Removed comments

--- src/main/java/com/example/demo/LevelOne.java
+++ src/main/java/com/example/demo/LevelOne.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 public class LevelOne extends LevelParent {
 
-	//private int enemiesSpawned = 0;
-
 	public LevelOne(double screenHeight, double screenWidth, HeartDisplay heartDisplay) {
 		super(GameConstants.BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, GameConstants.PLAYER_INITIAL_HEALTH, heartDisplay);
 	}
@@ -27,19 +25,6 @@ public class LevelOne extends LevelParent {
 		getRoot().getChildren().add(getUser());
 	}
 
-	/*@Override
-	protected void spawnEnemyUnits() {
-		int currentNumberOfEnemies = getCurrentNumberOfEnemies();
-		int enemiesToSpawn = GameConstants.TOTAL_ENEMIES - currentNumberOfEnemies;
-		for (int i = 0; i < enemiesToSpawn; i++) {
-			if (shouldSpawnEnemy()) {
-				double initialYPosition = generateRandomYPosition();
-				ActiveActorDestructible newEnemy = new EnemyPlane(getScreenWidth(), initialYPosition);
-				addEnemyUnit(newEnemy);
-			}
-		}
-	}*/
-
 	@Override
 	protected void spawnEnemyUnits() {
 		int currentNumberOfEnemies = getCurrentNumberOfEnemies();
@@ -51,20 +36,6 @@ public class LevelOne extends LevelParent {
 			}
 		}
 	}
-
-	/*@Override
-	protected void spawnEnemyUnits() {
-		// Only spawn new enemies if there are still enemies left to spawn
-		if (enemiesSpawned < GameConstants.TOTAL_ENEMIES) {
-			// Gradually spawn one enemy at a time with a certain probability
-			if (Math.random() < GameConstants.ENEMY_SPAWN_PROBABILITY) {
-				double initialYPosition = generateRandomYPosition();
-				ActiveActorDestructible newEnemy = new EnemyPlane(getScreenWidth(), initialYPosition);
-				addEnemyUnit(newEnemy);  // Add the new enemy to the game
-				enemiesSpawned++;  // Increment the number of enemies spawned
-			}
-		}
-	}*/
 
 	private boolean shouldSpawnEnemy() {
 		return Math.random() < GameConstants.ENEMY_SPAWN_PROBABILITY;
