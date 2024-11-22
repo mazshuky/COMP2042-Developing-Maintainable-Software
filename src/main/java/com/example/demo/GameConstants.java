10e09ea71bc672837770791ca0c9d5336b513d2b
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-22 13:52:45 +0800

    Changed the number of total enemies

--- src/main/java/com/example/demo/GameConstants.java
+++ src/main/java/com/example/demo/GameConstants.java
@@ -5,8 +5,8 @@ public class GameConstants {
     public static final int MILLISECOND_DELAY = 50;
     public static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
     public static final String NEXT_LEVEL = "com.example.demo.LevelTwo";
-    public static final int TOTAL_ENEMIES = 5;
-    public static final int KILLS_TO_ADVANCE = 10;
+    public static final int TOTAL_ENEMIES = 10;
+    public static final int KILLS_TO_ADVANCE = 5;
     public static final double ENEMY_SPAWN_PROBABILITY = .20;
     public static final int PLAYER_INITIAL_HEALTH = 5;
 
