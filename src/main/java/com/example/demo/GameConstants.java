1bf3b8a58ba54e3d8e30e1ff1d010e396ec607f8
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-26 21:06:23 +0800

    Renamed constants

--- src/main/java/com/example/demo/GameConstants.java
+++ src/main/java/com/example/demo/GameConstants.java
@@ -3,8 +3,8 @@ package com.example.demo;
 public class GameConstants {
     public static final double SCREEN_HEIGHT_ADJUSTMENT = 150;
     public static final int MILLISECOND_DELAY = 50;
-    public static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.jpg";
-    public static final String NEXT_LEVEL = "com.example.demo.LevelTwo";
+    public static final String BACKGROUND_IMAGE_ONE = "/com/example/demo/images/background1.jpg";
+    public static final String LEVEL_TWO = "com.example.demo.LevelTwo";
     public static final int TOTAL_ENEMIES = 10;
     public static final int KILLS_TO_ADVANCE = 5;
     public static final double ENEMY_SPAWN_PROBABILITY = .1;
