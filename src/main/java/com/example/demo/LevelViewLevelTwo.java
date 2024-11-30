f35f730f41fb2e6791741efd7dfadadfb516c155
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-30 16:01:55 +0800

    Adjust shield X position in LevelViewLevelTwo

--- src/main/java/com/example/demo/LevelViewLevelTwo.java
+++ src/main/java/com/example/demo/LevelViewLevelTwo.java
@@ -4,7 +4,7 @@ import javafx.scene.Group;
 
 public class LevelViewLevelTwo extends LevelView {
 
-	private static final int SHIELD_X_POSITION = 1150;
+	private static final int SHIELD_X_POSITION = 500;
 	private static final int SHIELD_Y_POSITION = 500;
 	private final Group root;
 	private final ShieldImage shieldImage;
