802627cec775aaf703c4b0ecb764ff28b6d3320d
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-11-30 12:12:29 +0800

    - Moved image initialization logic to a separate method `initializeImage` - Added error logging for scenarios where the image resource is not found

--- src/main/java/com/example/demo/WinImage.java
+++ src/main/java/com/example/demo/WinImage.java
@@ -4,23 +4,29 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class WinImage extends ImageView {
-	
+
 	private static final String IMAGE_NAME = "/com/example/demo/images/youwin.png";
 	private static final int HEIGHT = 500;
 	private static final int WIDTH = 600;
-	
+
 	public WinImage(double xPosition, double yPosition) {
-		var resource = getClass().getResource(IMAGE_NAME);
-		if (resource != null) {
-			setImage(new Image(resource.toExternalForm()));
-		}
+		initializeImage();
 		this.setVisible(false);
 		this.setFitHeight(HEIGHT);
 		this.setFitWidth(WIDTH);
 		this.setLayoutX(xPosition);
 		this.setLayoutY(yPosition);
 	}
-	
+
+	private void initializeImage() {
+		var resource = getClass().getResource(IMAGE_NAME);
+		if (resource != null) {
+			setImage(new Image(resource.toExternalForm()));
+		} else {
+			System.err.println("Resource not found: " + IMAGE_NAME);
+		}
+	}
+
 	public void showWinImage() {
 		this.setVisible(true);
 	}
