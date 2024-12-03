ebb5e5b7363b1b025d00a83f9e3427689661ac90
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-03 20:03:14 +0800

    adjusted images again

--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -15,9 +15,9 @@ public class UserPlane extends FighterPlane {
 	private static final double Y_LOWER_BOUND = 600.0;
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
-	private static final int IMAGE_HEIGHT = 150;
+	private static final int IMAGE_HEIGHT = 100;
 	private static final int VERTICAL_VELOCITY = 8;
-	private static final int PROJECTILE_X_POSITION = 90;
+	private static final int PROJECTILE_X_POSITION = 100;
 	private static final int PROJECTILE_Y_POSITION_OFFSET = -20;
 	private static final Logger logger = Logger.getLogger(UserPlane.class.getName());
 
