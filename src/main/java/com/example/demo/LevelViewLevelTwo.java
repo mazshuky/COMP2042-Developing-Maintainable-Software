7057fc2c0767eb0368773aba240f305eab1c5b97
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-03 14:20:44 +0800

    Add boss health bar and gets shield image

--- src/main/java/com/example/demo/LevelViewLevelTwo.java
+++ src/main/java/com/example/demo/LevelViewLevelTwo.java
@@ -8,17 +8,27 @@ public class LevelViewLevelTwo extends LevelView {
 	private static final int SHIELD_Y_POSITION = 500;
 	private final Group root;
 	private final ShieldImage shieldImage;
+	private final BossHealth bossHealthBar;
 
 	public LevelViewLevelTwo(Group root, int heartsToDisplay) {
 		super(root, heartsToDisplay);
 		this.root = root;
 		this.shieldImage = new ShieldImage(SHIELD_X_POSITION, SHIELD_Y_POSITION);
-		shieldImage.showShield();
+		this.bossHealthBar = new BossHealth();
 		addImagesToRoot();
 	}
 
 	private void addImagesToRoot() {
 		root.getChildren().addAll(shieldImage);
+		root.getChildren().addAll(bossHealthBar.getBossHealthBar(), bossHealthBar.getBossHealthLabel());
+	}
+
+	public void updateBossHealth(double health) {
+		bossHealthBar.update(health);
+	}
+
+	public ShieldImage getShieldImage() {
+		return shieldImage;
 	}
 
 }
