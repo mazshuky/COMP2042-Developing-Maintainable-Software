25eff2f61f3bc72a5e335a97f7d16a2636540392
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-03 20:39:11 +0800

    removed label for boss health bar

--- src/main/java/com/example/demo/BossHealth.java
+++ src/main/java/com/example/demo/BossHealth.java
@@ -1,24 +1,18 @@
 package com.example.demo;
 
-//import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 
 public class BossHealth {
     private final ProgressBar bossHealthBar;
-    //private Label bossHealthLabel;
 
     public BossHealth() {
         this.bossHealthBar = new ProgressBar();
-        //this.bossHealthLabel = new Label();
         bossHealthBar.setLayoutX(1070);
         bossHealthBar.setLayoutY(20);
         bossHealthBar.setPrefWidth(200);
         bossHealthBar.setPrefHeight(25);
         bossHealthBar.setProgress(1.0);
         bossHealthBar.setStyle("-fx-accent: green;");
-        //bossHealthLabel.setLayoutX(1000);
-        //bossHealthLabel.setLayoutY(0);
-       // bossHealthLabel.setText("Boss Health");
     }
 
     public void update(double health) {
@@ -29,8 +23,5 @@ public class BossHealth {
         return bossHealthBar;
     }
 
-    /*public Label getBossHealthLabel() {
-        return bossHealthLabel;
-    }*/
 }
 
