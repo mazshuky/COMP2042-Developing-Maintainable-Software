fb1598d4611f330c719617627233871873595e06
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-06 13:37:24 +0800

    Added javadoc comments

--- src/main/java/com/example/demo/ShieldImage.java
+++ src/main/java/com/example/demo/ShieldImage.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * ShieldImage class represents the shield image that appears on the screen.
+ */
 public class ShieldImage extends ImageView {
 	
 	private static final String IMAGE_NAME = "/com/example/demo/images/shield.png";
 	private static final int SHIELD_SIZE = 50;
-	
+
+	/**
+	 * Constructor for ShieldImage class.
+	 * @param xPosition the x-coordinate of the shield
+	 * @param yPosition the y-coordinate of the shield
+	 */
 	public ShieldImage(double xPosition, double yPosition) {
 		this.setLayoutX(xPosition);
 		this.setLayoutY(yPosition);
@@ -23,12 +31,18 @@ public class ShieldImage extends ImageView {
 		this.setFitWidth(SHIELD_SIZE);
 	}
 
+	/**
+	 * Shows the shield on the screen.
+	 */
 	public void showShield() {
 		System.out.println("Shield is now visible.");
 		this.setVisible(true);
 		this.toFront();
 	}
-	
+
+	/*
+	 * Hides the shield on the screen.
+	 */
 	public void hideShield() {
 		this.setVisible(false);
 	}
