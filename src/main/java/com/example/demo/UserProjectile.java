d3191a712a9fb46b3d6756df19e03bf001364cb0
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-06 13:55:39 +0800

    Added javadoc comments

--- src/main/java/com/example/demo/UserProjectile.java
+++ src/main/java/com/example/demo/UserProjectile.java
@@ -1,20 +1,34 @@
 package com.example.demo;
 
+/**
+ * CLass represents a projectile fired by the user.
+ */
 public class UserProjectile extends Projectile {
 
 	private static final String IMAGE_NAME = "userfire.png";
-	private static final int IMAGE_HEIGHT = 125;
+	private static final int IMAGE_HEIGHT = 30;
 	private static final int HORIZONTAL_VELOCITY = 15;
 
+	/**
+	 * Constructor for the UserProjectile class.
+	 * @param initialXPos the initial x position of the projectile
+	 * @param initialYPos the initial y position of the projectile
+	 */
 	public UserProjectile(double initialXPos, double initialYPos) {
-		super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
+		super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos + 30, initialYPos + 60);
 	}
 
+	/**
+	 * Updates the projectile's position by moving it horizontally.
+	 */
 	@Override
 	public void updatePosition() {
 		moveHorizontally(HORIZONTAL_VELOCITY);
 	}
-	
+
+	/**
+	 * Updates the actor's state. Currently, it only updates the position.
+	 */
 	@Override
 	public void updateActor() {
 		updatePosition();
