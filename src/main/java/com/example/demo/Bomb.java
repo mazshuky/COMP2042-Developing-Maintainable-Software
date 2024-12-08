b30de7bfa7c209e19b5baec6be8e09bf51a85ed3
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-08 17:03:47 +0800

    Introduced sound effect for bomb

--- src/main/java/com/example/demo/Bomb.java
+++ src/main/java/com/example/demo/Bomb.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.AudioClip;
 
 public class Bomb extends ImageView {
 
     private static final String IMAGE_NAME = "/com/example/demo/images/bomb.png";
     private static final int BOMB_SIZE = 70;
+
+    private AudioClip bombDropSound;
 
     public Bomb(double xPosition, double yPosition) {
         this.setLayoutX(xPosition);
@@ -19,9 +22,23 @@ public class Bomb extends ImageView {
         }
         this.setFitHeight(BOMB_SIZE);
         this.setPreserveRatio(true);
+        initializeSounds();
+    }
+
+    private void initializeSounds() {
+        this.bombDropSound = SoundEffects.loadSound("/com/example/demo/sounds/bombdrop.wav", this.getClass());
     }
 
     public void moveDown() {
         this.setLayoutY(this.getLayoutY() + 5);
+        playBombDropSound();
+    }
+
+    public void playBombDropSound() {
+        if (bombDropSound != null) {
+            bombDropSound.play();
+        } else {
+            System.out.println("Bomb drop sound is not initialized.");
+        }
     }
 }
