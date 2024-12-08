534646307ba09dcaa3de65d4863b7ea18970ad67
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-08 23:27:21 +0800

    Added JavaDoc comments

--- src/main/java/com/example/demo/WinImage.java
+++ src/main/java/com/example/demo/WinImage.java
@@ -3,12 +3,22 @@ package com.example.demo;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * The WinImage class represents an image that is displayed when the player wins the game.
+ * It extends the ImageView class from JavaFX.
+ */
 public class WinImage extends ImageView {
 
 	private static final String IMAGE_NAME = "/com/example/demo/images/youwin.png";
 	private static final int HEIGHT = 400;
 	private static final int WIDTH = 500;
 
+	/**
+	 * Constructs a WinImage object with the specified x and y positions.
+	 *
+	 * @param xPosition the x position
+	 * @param yPosition the y position
+	 */
 	public WinImage(double xPosition, double yPosition) {
 		initializeImage();
 		this.setVisible(false);
@@ -18,6 +28,10 @@ public class WinImage extends ImageView {
 		this.setLayoutY(yPosition);
 	}
 
+	/**
+	 * Initializes the image by loading it from the specified resource path.
+	 * If the resource is not found, an error message is printed to the standard error stream.
+	 */
 	private void initializeImage() {
 		var resource = getClass().getResource(IMAGE_NAME);
 		if (resource != null) {
@@ -27,6 +41,9 @@ public class WinImage extends ImageView {
 		}
 	}
 
+	/**
+	 * Makes the win image visible.
+	 */
 	public void showWinImage() {
 		this.setVisible(true);
 	}
