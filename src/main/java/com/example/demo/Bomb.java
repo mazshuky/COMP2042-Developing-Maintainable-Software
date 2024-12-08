c37338e6b265a09127c2c87ea4853acd082ac6b1
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-12-08 17:07:15 +0800

    Added JavaDoc comments

--- src/main/java/com/example/demo/Bomb.java
+++ src/main/java/com/example/demo/Bomb.java
@@ -4,6 +4,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.AudioClip;
 
+/**
+ * Represents a bomb object displaying on the screen and drops down.
+ * The bomb has an image representation and plays a sound when dropped.
+ */
 public class Bomb extends ImageView {
 
     private static final String IMAGE_NAME = "/com/example/demo/images/bomb.png";
@@ -11,6 +15,11 @@ public class Bomb extends ImageView {
 
     private AudioClip bombDropSound;
 
+    /**
+     * Constructor that initializes the bomb at a specific position on the screen.
+     * @param xPosition  the initial x-coordinate of the bomb
+     * @param yPosition  the initial y-coordinate of the bomb
+     */
     public Bomb(double xPosition, double yPosition) {
         this.setLayoutX(xPosition);
         this.setLayoutY(yPosition);
@@ -25,15 +34,24 @@ public class Bomb extends ImageView {
         initializeSounds();
     }
 
+    /*
+     * Initializes the sound effects for the bomb drop.
+     */
     private void initializeSounds() {
         this.bombDropSound = SoundEffects.loadSound("/com/example/demo/sounds/bombdrop.wav", this.getClass());
     }
 
+    /**
+     * Moves the bomb down by 5 pixels and plays the drop sound.
+     */
     public void moveDown() {
         this.setLayoutY(this.getLayoutY() + 5);
         playBombDropSound();
     }
 
+    /**
+     * Plays the bomb drop sound effect.
+     */
     public void playBombDropSound() {
         if (bombDropSound != null) {
             bombDropSound.play();
